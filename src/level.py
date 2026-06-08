a36bcad8c88ac7d6d993a15030f1c99f30abf454
Dmitry Luschan <dluschan@gmail.com>
2026-06-08 22:57:20 +0200

    refactoring: move main code to Game class

--- src/level.py
+++ src/level.py
@@ -1,3 +1,6 @@
+from pygame import Rect
+
+
 class Level:
     def __init__(self, width, start_x, start_y, platforms, finish_platform, ground, obstacles):
         self.width = width
@@ -7,3 +10,76 @@ class Level:
         self.finish_platform = finish_platform
         self.ground = ground
         self.obstacles = obstacles
+
+levels = [
+    Level(
+        width = 8000,
+        start_x = 100,
+        start_y = 100,
+        platforms = [
+            Rect(600, 450, 120, 10),
+            Rect(850, 350, 120, 10),
+            Rect(1010, 240, 120, 10),
+            Rect(1260, 400, 120, 10),
+            Rect(1480, 230, 120, 10),
+            Rect(1740, 300, 120, 10),
+            Rect(1940, 360, 120, 10),
+            Rect(2350 , 580, 150, 10),
+            Rect(2630 , 470, 120, 10),
+            Rect(2860, 350, 120, 10),
+            Rect(3100, 280, 120, 10),
+            Rect(3400, 200, 120, 10),
+            Rect(3650, 350, 120, 10),
+            Rect(3990, 200, 120, 10),
+            Rect(4190, 310, 120, 10),
+            Rect(4480, 420, 120, 10),
+            Rect(4780, 250, 120, 10),
+            Rect(5020, 100, 120, 10),
+            Rect(5280, 300, 120, 10),
+            Rect(5700, 270, 120, 10),
+            Rect(6120, 550, 210, 10),
+            Rect(6480, 360, 120, 10),
+            Rect(6750, 200, 120, 10),
+            Rect(7090, 270, 120, 10),
+            Rect(7090, 270, 120, 10),
+            Rect(7090, 270, 120, 10),
+            Rect(7490, 400, 120, 10),
+        ],
+        finish_platform = Rect(7860, 450, 140, 10),
+        ground = [Rect(0, 550, 600, 50)],
+        obstacles = [
+            Rect(420, 350, 100, 10),
+            Rect(1600, 150, 100, 10),
+            Rect(600, 700, 8400, 10),
+            Rect(2270, 350, 10, 120),
+            Rect(3820, 270, 10, 120),
+            Rect(600, 700, 3400, 10),
+            Rect(5480, 250, 10, 100),
+            Rect(5980, 130, 10, 100),
+        ]
+    ),
+    Level(
+        width = 13000,
+        start_x = 100,
+        start_y = 100,
+        platforms = [
+            Rect(600, 450, 120, 10),
+            Rect(850, 350, 120, 10),
+            Rect(1010, 240, 120, 10),
+            Rect(1260, 400, 120, 10),
+            Rect(1480, 230, 120, 10),
+            Rect(1740, 300, 120, 10),
+        ],
+        finish_platform = Rect(1860, 450, 140, 10),
+        ground = [
+            Rect(0, 550, 600, 50),
+            Rect(800, 550, 1200, 50),
+        ],
+        obstacles = [
+            Rect(420, 350, 100, 10),
+            Rect(1600, 150, 100, 10),
+            Rect(0, 700, 2000, 10),
+            Rect(600, 700, 3400, 10),
+        ]
+    )
+]
--- src/player.py
+++ src/player.py
@@ -1,3 +1,5 @@
+from dataclasses import field
+
 import pygame
 
 
@@ -6,6 +8,8 @@ class Player:
         self.player_image = pygame.image.load(img_file_name).convert_alpha()
         self.player_image = pygame.transform.scale_by(self.player_image, scale)
         self.frames = frames
+        self.moving = False
+        self.on_ground = False
 
         self.player_frames = []
         for i in range(frames):
@@ -16,25 +20,72 @@ class Player:
 
         self.rect = pygame.Rect(start_x, start_y, width * scale, height * scale)
         self.max_speed = 5
-        self.current_speed = 0
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def __str__(self):
+        return '\n'.join(f'{k} = {repr(v)}' for k, v in self.__dict__.items())
+
+    def apply_gravity(self, gravity):
+        # if not self.on_ground:
+        self.velocity_y += gravity
 
     def go_left(self):
-        self.current_speed = max(self.current_speed - 1, - self.max_speed)
-        self.rect.x += self.current_speed
+        self.moving = True
+        self.velocity_x = max(self.velocity_x - 1, - self.max_speed)
 
     def go_right(self):
-        self.current_speed = min(self.current_speed + 1, self.max_speed)
-        self.rect.x += self.current_speed
+        self.moving = True
+        self.velocity_x = min(self.velocity_x + 1, self.max_speed)
+
+    def stop(self):
+        self.moving = False
+
+    def vertical_hit(self, rect):
+        if self.velocity_y > 0:
+            self.land_on(rect)
+        else:
+            self.hit_ceiling(rect)
+
+    def land_on(self, rect):
+        self.rect.bottom = rect.top
+        self.velocity_y = 0
+        self.on_ground = True
+
+    def hit_ceiling(self, rect):
+        self.rect.top = rect.bottom
+        self.velocity_y = 0
+
+    def horizontal_hit(self, rect):
+        if self.velocity_x > 0:
+            self.rect.right = rect.left
+        else:
+            self.rect.left = rect.right
+
+    def jump(self):
+        if self.on_ground:
+            self.on_ground = False
+            self.velocity_y = -12
+
+    def fly(self):
+        self.on_ground = False
+
+    def vertical_update(self):
+        self.rect.y += self.velocity_y
+
+    def horizontal_update(self):
+        if not self.moving and self.velocity_x:
+            self.go_by_inertia()
+            self.next_frame()
+        elif self.moving:
+            self.next_frame()
+        self.rect.x += self.velocity_x
 
     def go_by_inertia(self):
-        if self.current_speed > 0:
-            self.current_speed -= 1
-        else:
-            self.current_speed += 1
-        self.rect.x += self.current_speed
-
-    def get_speed(self):
-        return self.current_speed
+        if self.velocity_x > 0:
+            self.velocity_x -= 1
+        elif self.velocity_x < 0:
+            self.velocity_x += 1
 
     def get_frame(self):
         # TODO pygame.transform.flip
