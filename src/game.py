19132125d7141ac44a67ef54adf52937c094fae6
Andrey <agamirovandrey@gmail.com>
2026-07-24 14:08:32 +0300

    fix the bug: reset platforms

--- src/game.py
+++ src/game.py
@@ -51,6 +51,9 @@ class Game:
         self.camera_x = 0
         self.time_left = self.level.time_left
 
+        for platform in self.level.platforms:
+            platform.reset()
+
     def update(self, dt):
         for platform in self.level.platforms:
             platform.update(dt, self.player)
